74490748d97a9adb888ada2dcc78a374769be66a
Einar Solbakken <43601639+einarsolbakken@users.noreply.github.com>
2022-03-16 14:25:46 +0100

    Add files via upload

---
+++ Motorvogn/src/main/java/Motorvogn.java
@@ -0,0 +1,45 @@
+
+
+
+public class Motorvogn {
+    private String personnr;
+    private String navn;
+    private String adresse;
+    private String kjennetegn;
+    private String bilmerke;
+    private String biltype;
+
+    //Konstruktør
+    public Motorvogn(String personnr, String navn, String adresse, String kjennetegn, String bilmerke, String biltype){
+        this.personnr = personnr;
+        this.navn = navn;
+        this.adresse = adresse;
+        this.kjennetegn = kjennetegn;
+        this.bilmerke = bilmerke;
+        this.biltype = biltype;
+    }
+
+    //TOm konstruktør
+    public Motorvogn(){}
+
+    //get / set - metoder
+    public String getPersonnr(){ return personnr;}
+    public  void setPersonnr(String personnr){this.personnr = personnr;}
+
+    public String getNavn(){ return navn;}
+    public  void setNavn(String navn){this.navn = navn;}
+
+    public String getAdresse(){ return adresse;}
+    public  void setAdresse(String adresse){this.adresse = adresse;}
+
+    public String getKjennetegn(){ return kjennetegn;}
+    public  void setKjennetegn(String kjennetegn){this.kjennetegn = kjennetegn;}
+
+    public String getBilmerke(){ return bilmerke;}
+    public  void setBilmerke(String bilmerke){this.bilmerke = bilmerke;}
+
+    public String getBiltype(){ return biltype;}
+    public  void setBiltype(String biltype){this.biltype = biltype;}
+
+
+}
---
+++ Motorvogn/src/test/java/com/example/motorvogn/MotorvognApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motorvogn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorvognApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
